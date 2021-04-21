4120d2f72d19dbdae98cdbe89e27a0aee6c1bbac
jagaillard <jgaillard@inflexsys.com>
2021-04-21 15:12:01 +0200

    test written

---
+++ testSquash.java
@@ -0,0 +1,8 @@
+public class testSquash {
+
+  public static int squash(int a, int b) {
+    int res = a + b;
+    
+    return res; 
+  }
+}
---
+++ testSquashTest.java
@@ -0,0 +1,8 @@
+import junit.framework.*;
+
+public class testSquashTest extends TestCase {
+
+  public void testCalculer() throws Exception {
+    assertTrue(1 == 1);
+  }
+}
